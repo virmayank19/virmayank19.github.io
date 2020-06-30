121643d3aa92d0b06bc0f78e148bd9554e41c787
virmayank19 <virmanimayank19@gmail.com>
2020-06-30 22:55:45 +0530

    Made Responsive

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// $(document).ready(function(){
+//     let hamburger=document.querySelector('.hamburger');
+//     let times = document.querySelector('.times');
+//     let mobilenav = document.querySelector('.mobile-nav');
+//     hamburger.addEventListener("click",function(){
+//         mobilenav.classList.add('open');
+//     })
+    
+//     times.addEventListener("click",function(){
+//         mobilenav.classList.remove('open');
+//     })
+// });
